bf57b65c4694fc7ff12a35181e4df29b40a5a373
decraenerobin <robin@esign.eu>
2024-08-14 11:24:19 +0200

    Update: footer styling

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import style from './footer.module.css';
-
+import ROUTES from '../../Routes/routes';
+import { Link, useLocation  } from 'react-router-dom';
+import { FaStar } from "react-icons/fa";
 
 const Footer = () => {
-  
-
+  const location = useLocation();
 
   return (
     <footer>
-
+      <div className={style.footerAlign}>
+        <div className={style.footerLeft}>
+          <Link to={ROUTES.home} className={location.pathname === ROUTES.home ? style.activeNav : ''}>Home</Link>
+          <Link to={ROUTES.projects} className={location.pathname === ROUTES.projects ? style.activeNav : ''}>Projects</Link>
+        </div>
+        <FaStar size={25}/>
+        <p>© 2024 Decraene Robin </p>
+      </div>
     </footer>
   );
 };
