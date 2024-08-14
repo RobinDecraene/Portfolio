0955b53d91c30fb350a9d8c4e3d6b04eb438e6e3
decraenerobin <robin@esign.eu>
2024-08-14 11:15:33 +0200

    Update: styling header

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import style from './header.module.css';
+import ROUTES from '../../Routes/routes';
+import { Link, useLocation  } from 'react-router-dom';
 
 
 const Header = () => {
+  const location = useLocation();
   
-
-
   return (
     <header>
       <nav>
-        
+        <a href="/">Robin</a>
+        <div className={style.navRight}>
+        <Link to={ROUTES.home} className={location.pathname === ROUTES.home ? style.activeNav : ''}>Home</Link>
+        <Link to={ROUTES.projects} className={location.pathname === ROUTES.projects ? style.activeNav : ''}>Projects</Link>
+        </div>
       </nav>
-
     </header>
   );
 };
