5da28b987351f95a2fb49c2ae427a744f0f178a0
decraenerobin <robin@esign.eu>
2024-08-20 12:03:50 +0200

    Update: icon

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import style from './header.module.css';
 import ROUTES from '../../Routes/routes';
 import { Link, useLocation  } from 'react-router-dom';
-
+import { FaStar } from "react-icons/fa";
 
 const Header = () => {
   const location = useLocation();
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <header>
       <nav>
-        <a href="/">Robin</a>
+        <a href="/"><FaStar size={25} color='#E0E1DD'/> Robin</a>
         <div className={style.navRight}>
         <Link to={ROUTES.home} className={location.pathname === ROUTES.home ? style.activeNav : ''}>Home</Link>
         <Link to={ROUTES.projects} className={location.pathname === ROUTES.projects ? style.activeNav : ''}>Projects</Link>
