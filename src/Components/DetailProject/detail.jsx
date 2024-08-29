241fafcf32d19a2a089747313d866b54e225d921
decraenerobin <robin@esign.eu>
2024-08-29 11:46:11 +0200

    add; detail page component

---
+++ src/Components/DetailProject/detail.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './detail.module.css';
+import Page from '../Page/page';
+
+const Detail = () => {
+  
+  return (
+    <Page>
+
+    </Page>
+  );
+};
+
+export default Detail;
