92a8135172b0b583be69a7d96c9e34ed79111a6e
decraenerobin <robin@esign.eu>
2024-08-29 11:46:27 +0200

    update: project page

--- src/Pages/Projects/projects.jsx
+++ src/Pages/Projects/projects.jsx
@@ -3,6 +3,7 @@ import style from './projects.module.css';
 import Title from '../../Components/Title/title';
 import Page from '../../Components/Page/page';
 import { firebase } from '../../firebase';
+import Card from '../../Components/Card/card';
 
 const Projects = () => {
   const [websites, setWebsites] = useState([]);
@@ -15,74 +16,73 @@ const Projects = () => {
         const querySnapshot = await firebase.firestore().collection('websites').get();
         const websitesArray = [];
         querySnapshot.forEach((doc) => {
-          websitesArray.push(doc.data());
+          websitesArray.push({ id: doc.id, ...doc.data() });
         });
         setWebsites(websitesArray);
       } catch (error) {
         console.error('Error getting documents:', error);
       }
     };
-
+  
     fetchWebsites();
-
+  
     const fetchDesigns = async () => {
       try {
         const querySnapshot = await firebase.firestore().collection('designs').get();
         const designsArray = [];
         querySnapshot.forEach((doc) => {
-          designsArray.push(doc.data());
+          designsArray.push({ id: doc.id, ...doc.data() });
         });
         setDesigns(designsArray);
       } catch (error) {
         console.error('Error getting documents:', error);
       }
     };
-
+  
     fetchDesigns();
-
+  
     const fetchOther = async () => {
       try {
         const querySnapshot = await firebase.firestore().collection('other').get();
         const otherArray = [];
         querySnapshot.forEach((doc) => {
-          otherArray.push(doc.data());
+          otherArray.push({ id: doc.id, ...doc.data() });
         });
         setOther(otherArray);
       } catch (error) {
         console.error('Error getting documents:', error);
       }
     };
-
+  
     fetchOther();
   }, []);
-
 
   return (
     <Page>
       <Title>Websites</Title>
       <div className={style.cards}>
-        {websites.map((index) => (
-          <div className={style.card} key={index}>
-            <p>Title</p>
-          </div>
+        {websites.map((website, index) => (
+          <Card key={index} link={`/detail/${website.id}`}>
+            <p>{website.title}</p>
+          </Card>
         ))}
       </div>
 
       <Title>Designs</Title>
       <div className={style.cards}>
-        {designs.map((index) => (
-          <div className={style.card} key={index}>
-            <p>Title</p>
-          </div>
+        {designs.map((design, index) => (
+          <Card key={index} link={`/detail/${design.id}`}>
+            <p>{design.title}</p>
+          </Card>
         ))}
       </div>
 
       <Title>Other</Title>
       <div className={style.cards}>
-        {other.map((index) => (
-          <div className={style.card} key={index}>
-            <p>Title</p>
-          </div>
+        {other.map(( other, index) => (
+          <Card key={index} link={`/detail/${other.id}`}>
+            <p>{other.title}</p>
+          </Card>
         ))}
       </div>
 
