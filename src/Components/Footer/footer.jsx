70620a176e59f05269841d1d50bda8989c1b4859
decraenerobin <robin@esign.eu>
2024-09-02 20:43:51 +0200

    Update: active nav

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import style from './footer.module.css';
 import ROUTES from '../../Routes/routes';
-import { Link, useLocation  } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FaStar } from "react-icons/fa";
 
 const Footer = () => {
   const location = useLocation();
+
+  const isProjectsActive = location.pathname.startsWith(ROUTES.projects);
 
   return (
     <footer>
       <div className={style.footerAlign}>
         <div className={style.footerLeft}>
           <Link to={ROUTES.home} className={location.pathname === ROUTES.home ? style.activeNav : ''}>Home</Link>
-          <Link to={ROUTES.projects} className={location.pathname === ROUTES.projects ? style.activeNav : ''}>Projects</Link>
+          <Link to={ROUTES.projects} className={isProjectsActive ? style.activeNav : ''}>Projects</Link>
         </div>
-        <FaStar size={25} color='#E0E1DD'/>
+        <FaStar size={25} color='#E0E1DD' />
         <p>© 2024 Decraene Robin </p>
       </div>
     </footer>
--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import style from './header.module.css';
 import ROUTES from '../../Routes/routes';
-import { Link, useLocation  } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FaStar } from "react-icons/fa";
 
 const Header = () => {
   const location = useLocation();
-  
+
+  const isProjectsActive = location.pathname.startsWith(ROUTES.projects);
+
   return (
     <header>
       <nav>
         <a href="/" className={style.name}><FaStar size={25} color='#E0E1DD' className={style.navStar}/>Robin</a>
         <div className={style.navRight}>
-        <Link to={ROUTES.home} className={location.pathname === ROUTES.home ? style.activeNav : ''}>Home</Link>
-        <Link to={ROUTES.projects} className={location.pathname === ROUTES.projects ? style.activeNav : ''}>Projects</Link>
+          <Link to={ROUTES.home} className={location.pathname === ROUTES.home ? style.activeNav : ''}>Home</Link>
+          <Link to={ROUTES.projects} className={isProjectsActive ? style.activeNav : ''}>Projects</Link>
         </div>
       </nav>
     </header>
