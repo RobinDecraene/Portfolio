436c3a2dc8768776350649c8a053e6ba8d78645e
decraenerobin <robin@esign.eu>
2024-09-05 12:15:25 +0200

    Add: card component

---
+++ src/Components/Card/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './card.module.css';
+
+const Card = ({children, link}) => {
+  return (
+    <a href={link} className={style.card}>
+      {children}
+    </a>
+  );
+};
+
+export default Card;
